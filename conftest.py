0f76bbe6f954939f5e9b7b596e426d31508f7cd6
Alex <kim.av9@gmail.com>
2024-05-21 16:21:48 +0500

    reorganization complete

--- conftest.py
+++ conftest.py
@@ -4,12 +4,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
-def browser():
+def browser(headless_flg=False):
     options = Options()
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_argument(r"--user-data-dir=C:\Users\MSI\AppData\Local\Google\Chrome\User Data")
     options.add_argument("--profile-directory=Profile 1")
+    if headless_flg:
+        options.add_argument('--headless')
     options.add_argument("start-maximized")
     chrome_browser = webdriver.Chrome(options=options)
     chrome_browser.implicitly_wait(10)

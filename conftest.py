fc3c930d86ff4c444252c4a827a8fb9dacc3440b
Alex <kim.av9@gmail.com>
2024-05-21 17:23:32 +0500

    fixed some issue

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
-def browser():
+def browser(headless_flg=False):
     options = Options()
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -12,7 +12,8 @@ def browser():
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36")
     options.add_argument(r"--user-data-dir=C:\Users\MSI\AppData\Local\Google\Chrome\User Data")
     options.add_argument("--profile-directory=Profile 1")
-    options.add_argument('--headless')
+    if (headless_flg):
+        options.add_argument('--headless')
     options.add_argument("start-maximized")
     chrome_browser = webdriver.Chrome(options=options)
     chrome_browser.implicitly_wait(10)
